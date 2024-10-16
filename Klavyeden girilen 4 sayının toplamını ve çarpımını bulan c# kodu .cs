1b61214f568c568cb3389f4b6c4a78713d9f9e9a
Gommor <67509066+Gommor@users.noreply.github.com>
2024-10-16 23:24:23 +0300

    Add files via upload

---
+++ Klavyeden girilen 4 sayının toplamını ve çarpımını bulan c# kodu .cs
@@ -0,0 +1,26 @@
+﻿int sayi1, sayi2, sayi3, sayi4, toplamlari, carpimlari;
+
+Console.WriteLine("Ilk sayi:");
+sayi1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ikinci sayi:");
+sayi2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ucuncu sayi:");
+sayi3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Dorduncu sayi:");
+sayi4 = int.Parse(Console.ReadLine());
+
+toplamlari = sayi1 + sayi2 + sayi3 + sayi4;
+carpimlari = sayi1 * sayi2 * sayi3 * sayi4;
+
+Console.WriteLine(" ");
+Console.WriteLine("Toplamlarinin Cevabi:" + toplamlari);
+Console.WriteLine(" ");
+Console.WriteLine("Carpimlarinin Cevabi:" + carpimlari);
+
+
+
+
+
---
+++ Klavyeden kenarları girilen dikdörtgenler prizmasının hacmini hesaplayan   c# kodu .cs
@@ -0,0 +1,21 @@
+﻿int a, b, c, alan;
+
+Console.WriteLine("Dikdörgenler prizmasının ilk kenar uzunluğu: ");
+a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Dikdörgenler prizmasının ikinci kenar uzunluğu: ");
+b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Dikdörgenler prizmasının üçüncü kenar uzunluğu: ");
+c = int.Parse(Console.ReadLine());
+
+alan = a * b * c;
+
+Console.WriteLine(" ");
+Console.WriteLine("Dikdörtgenler Prizmasi Alanı: " + alan);
+
+
+
+
+
+
