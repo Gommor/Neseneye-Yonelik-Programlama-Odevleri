9de183e4f40badada7aab1d17755e6c08a29ca97
Gommor <67509066+Gommor@users.noreply.github.com>
2024-11-09 01:20:46 +0300

    Add files via upload

---
+++ Ödev 3/Do While Döngüsü ile Basit Hesap Makinesi.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double sayi1, sayi2, sonuç;
+        char işlem;
+        string seçim;
+
+        do
+        {
+            Console.WriteLine("Birinci sayıyı giriniz:");
+            sayi1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("İkinci sayıyı giriniz:");
+            sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("İşlem seçin (+, -, *, /):");
+            işlem = Convert.ToChar(Console.ReadLine());
+
+            switch (işlem)
+            {
+                case '+':
+                    sonuç = sayi1 + sayi2;
+                    Console.WriteLine("Sonuç: " + sonuç);
+                    break;
+                case '-':
+                    sonuç = sayi1 - sayi2;
+                    Console.WriteLine("Sonuç: " + sonuç);
+                    break;
+                case '*':
+                    sonuç = sayi1 * sayi2;
+                    Console.WriteLine("Sonuç: " + sonuç);
+                    break;
+                case '/':
+                    if (sayi2 != 0)
+                    {
+                        sonuç = sayi1 / sayi2;
+
+                        Console.WriteLine("Sonuç: " + sonuç);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hata: Sıfıra bölme hatası.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz işlem.");
+                    break;
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Başka bir işlem yapmak istiyor musunuz? (e/cikis): ");
+                seçim = Console.ReadLine().ToLower();
+
+                if (seçim == "e")
+                {
+                    break;
+                }
+                else if (seçim == "cikis")
+                {      
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz giriş. Lütfen 'e' veya 'cikis' yazın.");
+                }
+            }
+
+        } while (seçim == "e");
+
+
+    }
+}
